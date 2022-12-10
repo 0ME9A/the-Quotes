fa0a41c57f2e9716f4fba47d05471d7b92ff183c
Baliram <heyome9a@gmail.com>
2022-12-10 10:47:15 +0000

    UI update

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-$(document).ready(function() {
-    $(".btn-local").css('background', 'transparent')
+$(document).ready(function () {
+    $('q').css('color', 'white')
+    $('cite').css('color', 'white')
+    $('#root').css('background', 'rgb(40, 40, 40)')
+    $('#next-btn ').css('background', 'rgb(40, 40, 40)')
+    $('#share-on-twitter').css('background', 'rgb(40, 40, 40)')
+
+
+
     const settings = {
         'async': true,
         'crossDomain': true,
         'url': "https://type.fit/api/quotes",
         'method': 'GET'
     }
-    const getReq = () =>{
-        $.ajax(settings).done(function(response){
+    const getReq = () => {
+        $.ajax(settings).done(function (response) {
             const data = JSON.parse(response)
-            let randomQuoto = data[Math.round(Math.random()*data.length)]
+            let randomQuoto = data[Math.round(Math.random() * data.length)]
             $("q").text(randomQuoto.text);
-            $('cite').text('-'+randomQuoto.author);
+            $('cite').text('-' + randomQuoto.author);
+            const twitter = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${randomQuoto.text}-"${randomQuoto.author}"`
+            $("#share-on-twitter").attr("href", twitter)
         })
     }
     getReq()
 
-    $('#next').click(function (e) { 
+    $('body #next-btn').click(function (e) {
+        const x = Math.random() * 255;
+        const y = Math.random() * 255;
+        const z = Math.random() * 255;
+        const rgb = `rgba(${x},${y},${z},.5)`
+        $('q').css('color', rgb)
+        $('cite').css('color', rgb)
+        $('#root').css('background', rgb)
+        $('#next-btn ').css('background', rgb)
+        $('#share-on-twitter').css('background', rgb)
         $("article").css('opacity', 0);
+
         e.preventDefault();
         setTimeout(() => {
             getReq()
@@ -26,13 +45,4 @@ $(document).ready(function() {
             $("article").css('opacity', 1);
         }, 1500);
     });
-
-
-    setInterval(() => {
-        const x = Math.random()*255;
-        const y = Math.random()*255;
-        const z = Math.random()*255;
-        const rgb = `rgba(${x},${y},${z},.5)`
-        $("nav").parent().css('background',rgb)
-    }, 1000);
 })
